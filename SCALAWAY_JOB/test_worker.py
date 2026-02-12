82a0f76c89729226584abe99f12da295632f6ec9
Ibrahim Masalha <ibrahim@masalha.me>
2026-02-12 10:18:19 +0100

    pre algos

--- SCALAWAY_JOB/test_worker.py
+++
@@ -1,9 +0,0 @@
-import json 
-from pipeline.worker import run_one_job
-from dotenv import load_dotenv
-load_dotenv("vm.env") 
-
-
-payload=json.load(open('payload.json','r',encoding='utf-8'))
-
-print(run_one_job(payload)) 
